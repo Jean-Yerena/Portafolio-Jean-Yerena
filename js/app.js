cd9fcaf845c5b14ee0c769fc43051a20d5f8bbac
Jean-Yerena <jfyerenaq@miusbctg.edu.co>
2023-04-06 10:58:01 -0500

    Primera parte responsive

--- js/app.js
+++ js/app.js
@@ -1,27 +1,52 @@
 //Efecto hacer visible el header
 const header = document.querySelector('#header');
 window.addEventListener('scroll', () => {
-    header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle('sticky', window.scrollY > 100);
 })
 
 //Codigo para que funcione el formulario
 const btn = document.querySelector('.btn_form');
 
 document.querySelector('.contact_form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_dv1368q';
+    const serviceID = 'default_service';
+    const templateID = 'template_dv1368q';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
+
+//Cambiar texto en width portatil
+const parrafo = document.querySelector('.parrafoAbout');
+
+
+
+
+cambiarTexto();
+
+function cambiarTexto() {
+  var width = document.documentElement.clientWidth;
+
+  if(width <= 1366) {
+    parrafo.innerHTML = `<p class="parrafoAbout">Hola, ¡bienvenido a mi página personal! Mi nombre es Jean Yerena, y soy un desarrollador
+    frontend junior apasionado por crear experiencias digitales atractivas y accesibles. <br>
+
+    Siempre he tenido una gran curiosidad por la tecnología y el diseño. Me encanta trabajar con HTML, CSS y
+    JavaScript para crear páginas web y aplicaciones que no solo sean funcionales, sino también hermosas y
+    fáciles de usar. <br>
+
+    he estado trabajando duro para mejorar mis habilidades y adquirir nuevas herramientas y conocimientos,
+    estoy emocionado por la oportunidad de aprender y crecer en esta industria en constante evolución.
+</p>`;
+  }
+}
